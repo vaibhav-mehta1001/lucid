33cf6b768ae2e1ccab9a9ef333ca973088095314
Your Name <vaibhavmehta1001@gmail.com>
2022-06-28 16:58:47 -0400

    added inlining opt

---
+++ src/lib/frontend/transformations/EventInlining.ml
@@ -0,0 +1,215 @@
+(* An inlining pass for certain kinds of functions / 
+   calls that are easy to inline with zero overhead. 
+   To be inlined with no overhead, there are two requirements: 
+    1. the function never writes to its parameters
+    2. the function call appears in an assignment statement, 
+       OR the function has only one return statement.
+   This pass runs before the general inlining pass and does not 
+   remove any function definitions from the program.
+   This pass will eventually be removed when either function 
+   inlining is improved or partial interpretation is complete and 
+   optimized away the overhead added by inlining. 
+   - jps 1/18/22
+*)
+
+open Syntax
+open Batteries
+open Collections
+
+let err s = Console.error s
+
+(* get data about functions in declarations *)
+let dfuns ds = 
+  let is_fcn d = match d.d with
+    | DHandler(id, (params,stmt)) -> Some (id, params, stmt)
+    (* | DFun(id, _, _, (params, stmt)) -> Some (id, params, stmt) *)
+    | _ -> None
+  in 
+  List.filter_map is_fcn ds
+;;
+
+(* Check if a variable is never assigned in s. *)
+let read_only_var (id : Id.t) (s : statement) : bool =
+  let ret = ref true in
+  let v =
+    object
+      inherit [_] s_iter as super
+      method! visit_SAssign _ id2 _ = if Id.equal id id2 then ret := false
+    end
+  in
+  v#visit_statement () s;
+  !ret
+;;
+
+(* Check if no parameters are assigned to in a function *)
+let read_only_params fcn_params fcn_stmt = 
+  List.fold_left (fun prev p -> prev & (read_only_var (fst p) fcn_stmt)) true fcn_params 
+;;
+
+let count_returns fcn_stmt =
+  let num_rets = ref 0 in 
+  let finder = 
+    object
+      inherit [_] s_iter as super
+
+      method ! visit_SRet () _ = 
+        num_rets := !num_rets + 1;
+    end
+  in 
+  finder#visit_statement () fcn_stmt;
+  !num_rets
+
+
+let single_return fcn_stmt = 
+  (count_returns fcn_stmt) = 1
+;;
+
+let no_return fcn_stmt = 
+  (count_returns fcn_stmt) = 0
+;;
+
+
+(* replace each parameter with its argument. This should only be caled 
+   for functions with read_only_params! *)
+let replace_params_with_args stmt (params:(id * ty) list) (args: exp list) = 
+  let subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_EVar (env : e IdMap.t) x =
+        match IdMap.find_opt (Cid.to_id x) env with
+        | Some e -> e
+        | None -> EVar x
+    end
+  in 
+  (* create map from id (param) to expression (arg) *)
+  let id_exp_map = List.fold_left 
+    (fun idmap (param, arg) ->  
+      IdMap.add (fst param) arg.e idmap)
+    IdMap.empty
+    (List.combine params args)
+  in 
+  (* do all the substitutions *)
+  subst#visit_statement id_exp_map stmt
+;;
+
+(* replace return statements with assignments and delete expression-less returns. 
+   This should only be called for function call assignments! *)
+let replace_return_with_assign id stmt = 
+  let ret_subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_SRet (id : Id.t) exp_opt =
+        match exp_opt with 
+          | None -> SNoop
+          | Some exp -> 
+            SAssign (id,exp)
+    end
+  in
+  ret_subst#visit_statement id stmt
+;;
+let replace_return_with_local id ty stmt = 
+  let ret_subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_SRet (id : Id.t) exp_opt =
+        match exp_opt with 
+          | None -> SNoop
+          | Some exp -> 
+            SLocal (id,ty,exp)
+    end
+  in
+  ret_subst#visit_statement id stmt
+;;
+(* Inline functions that have multiple return statements 
+   and immutable variables wherever possible. *)
+let inline_multi_return_immutable (ds:decl list) ((fcn_id:Id.t),(fcn_params : params),fcn_stmt)  = 
+  match (read_only_params fcn_params fcn_stmt) with 
+  | true -> 
+    (* print_endline (("[inline_multi_return_immutable] all parameters immutable for function:"^(Id.to_string fcn_id))); *)
+    let assign_subst = 
+      object
+        inherit [_] s_map as super
+        method! visit_SAssign (fcn_id, fcn_params, fcn_stmt) id exp = 
+          (* print_endline ("[inline_multi_return_immutable] visiting assign."); *)
+          (* Printing.exp_to_string exp |> print_endline; *)
+          match exp.e with 
+          | ECall(cid, args) -> (
+            (* print_endline ("[inline_multi_return_immutable] call in assign: "^(Cid.to_string cid)); *)
+            match (Cid.equal (Cid.id fcn_id) cid) with 
+            | true ->
+              (* an assignment that calls a function with read only params. 
+                we can inline this with zero overhead. *)
+              (* step 1: replace each parameter in the function body with 
+                         the appropriate argument. *)
+              let fcn_stmt = replace_params_with_args fcn_stmt fcn_params args in 
+              (* step 2: replace each return statement with an assignment to 
+                         the caller's variable. *)
+              let fcn_stmt = replace_return_with_assign id fcn_stmt in 
+              (* step 3: return the transformed function body instead of the 
+                         call. *)
+              fcn_stmt.s
+            | false -> SAssign(id, exp) (* call to different function *)
+          )
+          (* not a call *)
+          | _ -> SAssign(id, exp)      
+      end
+    in 
+    assign_subst#visit_decls (fcn_id, fcn_params, fcn_stmt) ds
+  | false -> 
+    (* print_endline ("[inline_multi_return_immutable] cannot inline -- mutable parameter."); *)
+  ds (* some of the parameters in the function are written *)
+;;
+
+let can_inline fcn_stmt = 
+ let rec count_stmts i s = 
+  match s with 
+  | SSeq(x, next) -> count_stmts (i+1) next
+  | _ -> (i+1)
+  in count_stmts 0 fcn_stmt
+
+(* Inline functions that have one return statement and immutable variables 
+   anywhere they appear. *)
+let inline_no_return_or_single_return_immutable (ds:decl list) ((fcn_id:Id.t),(fcn_params : params),fcn_stmt)  = 
+  match (can_inline fcn_stmt) with 
+  | true -> 
+    let assign_subst = 
+      object
+        inherit [_] s_map as super
+        method! visit_statement (fcn_id, fcn_params, fcn_stmt) stmt = 
+          let stmt = match stmt.s with 
+            (* for unit calls to functions with read only params, we can just 
+               replace params and be done. *)
+            | SGen(_, exp) -> (
+              match exp.e with 
+                | ECall(cid, args) -> (
+                  match (Cid.equal (Cid.id fcn_id) cid) with 
+                  | true -> 
+                    let fcn_stmt = replace_params_with_args fcn_stmt fcn_params args in 
+                    fcn_stmt
+                  | false -> stmt
+                )
+                | _ -> stmt
+            )
+            | _ -> stmt
+          in 
+          super#visit_statement (fcn_id, fcn_params, fcn_stmt) stmt
+      end
+    in 
+    assign_subst#visit_decls (fcn_id, fcn_params, fcn_stmt) ds
+  | false -> ds (* some of the parameters in the function are written *)
+;;
+
+(* zero-overhead inlining pass for a certain subset of functions / calls. *)
+let inline_prog_specialcase ds =
+  (* try zero-overhead inlining all function calls in assignment 
+     statements. To be eligible, the function call's parameters must 
+     be immutable.*)
+  (* let ds = List.fold_left inline_multi_return_immutable ds (dfuns ds) in  *)
+  (* all functions with 1 return statement and immutable parameters can 
+  be inlined with 0 overhead, regardless of where they appear. *)
+  let ds = List.fold_left inline_no_return_or_single_return_immutable ds (dfuns ds) in 
+  (* finally, inline all function calls in unit statements with immutable parameters *)
+  ds
--- src/lib/frontend/transformations/LucidLogTransformation.ml
+++ src/lib/frontend/transformations/LucidLogTransformation.ml
@@ -4,6 +4,204 @@ open Batteries
 open Collections
 open Printing
 
+let err s = Console.error s
+
+(* get data about functions in declarations *)
+let dfuns ds = 
+  let is_fcn d = match d.d with
+    | DHandler(id, (params,stmt)) -> Some (id, params, stmt)
+    (* | DFun(id, _, _, (params, stmt)) -> Some (id, params, stmt) *)
+    | _ -> None
+  in 
+  List.filter_map is_fcn ds
+;;
+
+(* Check if a variable is never assigned in s. *)
+let read_only_var (id : Id.t) (s : statement) : bool =
+  let ret = ref true in
+  let v =
+    object
+      inherit [_] s_iter as super
+      method! visit_SAssign _ id2 _ = if Id.equal id id2 then ret := false
+    end
+  in
+  v#visit_statement () s;
+  !ret
+;;
+
+(* Check if no parameters are assigned to in a function *)
+let read_only_params fcn_params fcn_stmt = 
+  List.fold_left (fun prev p -> prev & (read_only_var (fst p) fcn_stmt)) true fcn_params 
+;;
+
+let count_returns fcn_stmt =
+  let num_rets = ref 0 in 
+  let finder = 
+    object
+      inherit [_] s_iter as super
+
+      method ! visit_SRet () _ = 
+        num_rets := !num_rets + 1;
+    end
+  in 
+  finder#visit_statement () fcn_stmt;
+  !num_rets
+
+
+let single_return fcn_stmt = 
+  (count_returns fcn_stmt) = 1
+;;
+
+let no_return fcn_stmt = 
+  (count_returns fcn_stmt) = 0
+;;
+
+
+(* replace each parameter with its argument. This should only be caled 
+   for functions with read_only_params! *)
+let replace_params_with_args stmt (params:(id * ty) list) (args: exp list) = 
+  let subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_EVar (env : e IdMap.t) x =
+        match IdMap.find_opt (Cid.to_id x) env with
+        | Some e -> e
+        | None -> EVar x
+    end
+  in 
+  (* create map from id (param) to expression (arg) *)
+  let id_exp_map = List.fold_left 
+    (fun idmap (param, arg) ->  
+      IdMap.add (fst param) arg.e idmap)
+    IdMap.empty
+    (List.combine params args)
+  in 
+  (* do all the substitutions *)
+  subst#visit_statement id_exp_map stmt
+;;
+
+(* replace return statements with assignments and delete expression-less returns. 
+   This should only be called for function call assignments! *)
+let replace_return_with_assign id stmt = 
+  let ret_subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_SRet (id : Id.t) exp_opt =
+        match exp_opt with 
+          | None -> SNoop
+          | Some exp -> 
+            SAssign (id,exp)
+    end
+  in
+  ret_subst#visit_statement id stmt
+;;
+let replace_return_with_local id ty stmt = 
+  let ret_subst =
+    object
+      inherit [_] s_map as super
+
+      method! visit_SRet (id : Id.t) exp_opt =
+        match exp_opt with 
+          | None -> SNoop
+          | Some exp -> 
+            SLocal (id,ty,exp)
+    end
+  in
+  ret_subst#visit_statement id stmt
+;;
+(* Inline functions that have multiple return statements 
+   and immutable variables wherever possible. *)
+let inline_multi_return_immutable (ds:decl list) ((fcn_id:Id.t),(fcn_params : params),fcn_stmt)  = 
+  match (read_only_params fcn_params fcn_stmt) with 
+  | true -> 
+    (* print_endline (("[inline_multi_return_immutable] all parameters immutable for function:"^(Id.to_string fcn_id))); *)
+    let assign_subst = 
+      object
+        inherit [_] s_map as super
+        method! visit_SAssign (fcn_id, fcn_params, fcn_stmt) id exp = 
+          (* print_endline ("[inline_multi_return_immutable] visiting assign."); *)
+          (* Printing.exp_to_string exp |> print_endline; *)
+          match exp.e with 
+          | ECall(cid, args) -> (
+            (* print_endline ("[inline_multi_return_immutable] call in assign: "^(Cid.to_string cid)); *)
+            match (Cid.equal (Cid.id fcn_id) cid) with 
+            | true ->
+              (* an assignment that calls a function with read only params. 
+                we can inline this with zero overhead. *)
+              (* step 1: replace each parameter in the function body with 
+                         the appropriate argument. *)
+              let fcn_stmt = replace_params_with_args fcn_stmt fcn_params args in 
+              (* step 2: replace each return statement with an assignment to 
+                         the caller's variable. *)
+              let fcn_stmt = replace_return_with_assign id fcn_stmt in 
+              (* step 3: return the transformed function body instead of the 
+                         call. *)
+              fcn_stmt.s
+            | false -> SAssign(id, exp) (* call to different function *)
+          )
+          (* not a call *)
+          | _ -> SAssign(id, exp)      
+      end
+    in 
+    assign_subst#visit_decls (fcn_id, fcn_params, fcn_stmt) ds
+  | false -> 
+    (* print_endline ("[inline_multi_return_immutable] cannot inline -- mutable parameter."); *)
+  ds (* some of the parameters in the function are written *)
+;;
+
+let can_inline fcn_stmt = 
+ let rec count_stmts i s = 
+  match s with 
+  | SSeq(x, next) -> count_stmts (i+1) (next.s)
+  | _ -> (i+1)
+  in (count_stmts 0 fcn_stmt.s) < 3
+
+(* Inline functions that have one return statement and immutable variables 
+   anywhere they appear. *)
+let inline_no_return_or_single_return_immutable (ds:decl list) ((fcn_id:Id.t),(fcn_params : params),fcn_stmt)  = 
+  match (can_inline fcn_stmt) with 
+  | true -> let _ = print_string "Should inline" in 
+    let assign_subst = 
+      object
+        inherit [_] s_map as super
+        method! visit_statement (fcn_id, fcn_params, fcn_stmt) stmt = 
+          let stmt = match stmt.s with 
+            (* for unit calls to functions with read only params, we can just 
+               replace params and be done. *)
+            | SGen(_, exp) -> (
+              match exp.e with 
+                | ECall(cid, args) -> (
+                  match (Cid.equal (Cid.id fcn_id) cid) with 
+                  | true -> 
+                    let fcn_stmt = replace_params_with_args fcn_stmt fcn_params args in 
+                    fcn_stmt
+                  | false -> stmt
+                )
+                | _ -> stmt
+            )
+            | _ -> stmt
+          in 
+          super#visit_statement (fcn_id, fcn_params, fcn_stmt) stmt
+      end
+    in 
+    assign_subst#visit_decls (fcn_id, fcn_params, fcn_stmt) ds
+  | false -> ds (* some of the parameters in the function are written *)
+;;
+
+(* zero-overhead inlining pass for a certain subset of functions / calls. *)
+let inline_prog_specialcase ds =
+  (* try zero-overhead inlining all function calls in assignment 
+     statements. To be eligible, the function call's parameters must 
+     be immutable.*)
+  (* let ds = List.fold_left inline_multi_return_immutable ds (dfuns ds) in  *)
+  (* all functions with 1 return statement and immutable parameters can 
+  be inlined with 0 overhead, regardless of where they appear. *)
+  let ds = List.fold_left inline_no_return_or_single_return_immutable ds (dfuns ds) in 
+  (* finally, inline all function calls in unit statements with immutable parameters *)
+  ds
+
 let to_stmt s = 
  {s=s;sspan = Span.default}
 
@@ -130,7 +328,7 @@ in stmt :: stmt2
         
 (*ID is table name *)
 let rec generate_table_body id graphs params rule_ctxt table_ctxt rule_args_ctxt arr_names loc = 
- let generate_deps d = 
+ let generate_deps can_inline d = 
   List.map(fun name ->  let _ = print_string (fst(name)) in
               let args = get_args_rule name id params rule_args_ctxt table_ctxt rule_ctxt
               in match args with 
@@ -145,10 +343,11 @@ let rec generate_table_body id graphs params rule_ctxt table_ctxt rule_args_ctxt
  in let deps = match (List.assoc_opt id graphs)
   with | Some dep -> dep (*dep is rule name *)
        | None -> [] in 
-let stmts_set  = compile_set_table table_ctxt id params arr_names loc in 
+let stmts_set  = compile_set_table table_ctxt id params arr_names loc in
 let stmts_set = List.map to_stmt stmts_set in 
+  let can_inline = ((List.length stmts_set) = 0) && (List.length deps) <= 1 in
  let stmts = List.fold_left(fun acc x-> 
-                  {s=SSeq(acc, x);sspan=Span.default}) {s=SNoop;sspan=Span.default} (stmts_set @ (generate_deps deps)) in DHandler((("event_"^(fst(id))), snd(id)), (params, (stmts)))
+                  {s=SSeq(acc, x);sspan=Span.default}) {s=SNoop;sspan=Span.default} (stmts_set @ (generate_deps can_inline deps)) in DHandler((("event_"^(fst(id))), snd(id)), (params, (stmts)))
 
 let generate_val_tables_ctxt decl =
   match decl.d with 
@@ -181,7 +380,6 @@ let generate_table graph (vals_ctxt)  rule_ctxt table_ctxt rule_args  d = match 
                          [DEvent((("event_" ^ (fst name)), (snd name)), EEntry(true), [], params );
                           (generate_table_body name graph params rule_ctxt table_ctxt rule_args (List.assoc name vals_ctxt)) (snd(param))]
                          | _ -> []
-
 
 (* MAP from table name to keys  *)
 let tbl_arg_ctxt (decls : decls) = 
@@ -295,8 +493,7 @@ let compile_lookup  (name : cid) arr_names i exp =
   | _ -> failwith "bad arg"
 
 let rec lastk ls k i = 
-match ls with 
-| [] -> []
+match ls with | [] -> []
 | _ :: tl -> if i = k then tl else lastk tl k (i+1)
 (* 
 let get_indices_2 params keys = 
@@ -417,7 +614,7 @@ let f acc s =
 (* Compile Body Event *)
 
 
-let compile_handler_body vals_ctxt rule_args 
+let compile_handler_body  vals_ctxt rule_args 
 (tbl_arg_ctxt : (Syntax.id * (Syntax.id * Syntax.ty) list) list) rule = 
 match rule.d with 
 | DMin(n, DRule {lhs=Table{name; loc; args}; preds; exps; stmt;agg}) -> 
@@ -438,6 +635,30 @@ let stmts = (List.map to_stmt stmts) @
 [to_stmt (SIf(exps, combine_stmt (stmt @ (List.map to_stmt b1)), to_stmt SNoop))] in 
 let body_stmt = combine_stmt stmts 
 in [DHandler(n, (params,body_stmt))]
+ 
+| _ -> []
+
+let compile_handler_body_ctxt  vals_ctxt rule_args 
+(tbl_arg_ctxt : (Syntax.id * (Syntax.id * Syntax.ty) list) list) rule = 
+match rule.d with 
+| DMin(n, DRule {lhs=Table{name; loc; args}; preds; exps; stmt;agg}) -> 
+ let keys = List.assoc name tbl_arg_ctxt in 
+  let _ = print_keys keys in 
+ let arr_names = List.assoc name vals_ctxt in 
+ let _ = print_vals arr_names in 
+ let params = List.assoc n rule_args in 
+ let stmts = List.flatten (List.map(compile_pred params vals_ctxt tbl_arg_ctxt) preds)
+ in let exps = if (List.length exps > 1) then 
+  {e=(List.fold_left compile_exps (EOp(And, [])) exps);ety=None; espan=Span.default} else 
+  (List.nth exps 0) 
+in let args = begin match loc with Some x -> {e=EVar(Id x);ety=None;espan=Span.default}
+ :: args | None -> args end in 
+  let has_loc = begin match loc with Some _ -> true | None -> false end in 
+let b1 = compile_true_branch keys args arr_names has_loc agg in 
+let stmts = (List.map to_stmt stmts) @ 
+[to_stmt (SIf(exps, combine_stmt (stmt @ (List.map to_stmt b1)), to_stmt SNoop))] in 
+let body_stmt = combine_stmt stmts 
+in [(fst n), DHandler(n, (params,body_stmt))]
  
 | _ -> []
 (*  
@@ -485,18 +706,18 @@ let process_prog (decl : decls) : decls =
     let _ = print_string "\n Graph DOne \n" in 
     let prog =  List.flatten (List.map (create_rule_event rule_args) decl) in
     let prog = prog @ List.flatten
-     (List.map (generate_table graph  vals_ctxt rule_mapping table_args rule_args) decl) in 
+     (List.map (generate_table graph  vals_ctxt rule_mapping table_args rule_args) decl) in
     let prog = prog @ List.flatten (List.map (compile_handler_body vals_ctxt rule_args table_args) decl)
     in 
     (* let prog = prog @ List.flatten (List.map (create_rule_event pctxt) decl) *)
    let prog = List.map(fun x -> {d=x;dspan=Span.default}) prog
-   in  
-    
+   in let ds = 
    [{d=DConst(("SELF",0), 
    {raw_ty=TInt(IConst 32);teffect=FZero; tspan=Span.default;
    tprint_as=ref None}, {e=EVal({v=(VInt (Integer.of_int 0));vty=None;vspan=Span.default});ety=None;espan=Span.default});dspan=Span.default};
    {d=DConst(("SEED",0), 
    {raw_ty=TInt(IConst 32);teffect=FZero; tspan=Span.default;
    tprint_as=ref None}, {e=EVal({v=(VInt (Integer.of_int 2048));vty=None;vspan=Span.default});ety=None;espan=Span.default});dspan=Span.default}]
-    @ prog @ (remove decl) 
+    @ prog @ (remove decl) in 
+    inline_prog_specialcase ds 
 
